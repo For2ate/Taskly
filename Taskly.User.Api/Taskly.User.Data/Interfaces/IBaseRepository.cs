2a0afda627921ecba663140e305b030dc9540ae3
Ivan_Skrypinski <ivanskrypinski@gmail.com>
2025-02-11 18:10:00 +0300

    Add basic arch of UserApi

---
+++ Taskly.User.Api/Taskly.User.Data/Interfaces/IBaseRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Taskly.User.Data.Entities;
+
+namespace Taskly.User.Data.Interfaces {
+    
+    public interface IBaseRepository <TEntity> where TEntity : BaseEntity {
+
+        Task <TEntity> GetByIdAsync(Guid id);
+
+        Task <IEnumerable <TEntity> > GetAllAsync();
+
+        Task AddAsync(TEntity entity);
+
+        Task UpdateAsync(TEntity entity);
+
+        Task RemoveAsync(TEntity entity);
+
+    }
+
+}
